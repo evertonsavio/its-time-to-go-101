075a0b4f59e9e34c49f60270e6f838b973ac2867
havyx <everluca@outlook.com>
2021-04-03 12:20:33 -0300

    :sparkles: web server

---
+++ http/server.go
@@ -0,0 +1,26 @@
+package http
+
+import (
+	"net/http"
+
+	"github.com/havyx/Golang-Its-time-to-go/model"
+	"github.com/labstack/echo/v4"
+)
+
+type WebServer struct {
+	Products *model.Products
+}
+
+func NewWebServer() *WebServer {
+	return &WebServer{}
+}
+
+func (w WebServer) Serve() {
+	e := echo.new()
+	e.GET("/product", w.getAll)
+	e.Logger.Fatal(e.start(":8585"))
+}
+
+func (w WebServer) getAll(c echo.Context) error {
+	return c.JSON(http.StatusOK, w.Products)
+}
--- main.go
+++ main.go
@@ -1,1 +1,12 @@
 package main
+
+import "net/http"
+
+func main() {
+	http.HandleFunc("/product", Producthandler)
+	http.ListenAndServe("9898", nil)
+}
+
+func Producthandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h3>Hello World</h3>"))
+}
